62c14526a9e18babc6601582d84920bad2a1fd0e
Yathartha <tuladhay@oregonstate.edu>
2019-01-09 23:22:49 -0800

    removed VAE. Now comm is generated by feedforward nn

--- main.py
+++ main.py
@@ -184,14 +184,14 @@ if __name__ == '__main__':
     parser.add_argument("--n_rollout_threads", default=1, type=int)
     parser.add_argument("--n_training_threads", default=6, type=int)
     parser.add_argument("--buffer_length", default=int(1e6), type=int)
-    parser.add_argument("--n_episodes", default=50000, type=int)
+    parser.add_argument("--n_episodes", default=100000, type=int)
     parser.add_argument("--episode_length", default=60, type=int)
     parser.add_argument("--steps_per_update", default=100, type=int)
     parser.add_argument("--batch_size",
                         default=1024, type=int,
                         help="Batch size for model training")
-    parser.add_argument("--n_exploration_eps", default=50000, type=int)
-    parser.add_argument("--init_noise_scale", default=0.2, type=float)
+    parser.add_argument("--n_exploration_eps", default=100000, type=int)
+    parser.add_argument("--init_noise_scale", default=0.3, type=float)
     parser.add_argument("--final_noise_scale", default=0.0, type=float)
     parser.add_argument("--save_interval", default=2000, type=int)
     parser.add_argument("--hidden_dim", default=64, type=int)
